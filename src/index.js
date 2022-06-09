58e5d5b8fbded546a4931c333b0f6e5dd48bb329
Hitesh Verma <hitesh.verma2711@gmail.com>
2022-06-09 12:42:17 +0530

    fixed routing

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { UserProvider } from "./context/userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
 
